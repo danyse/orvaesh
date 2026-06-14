b9d0a30850d1a7c6d17ef237a9e5533f2fc74579
danyse <danyclassique@gmail.com>
2026-06-14 12:47:46 +0300

    14062026-3

--- src/app/components/home-view.component/home-view.component.ts
+++ src/app/components/home-view.component/home-view.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild  } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home-view',
@@ -9,30 +10,42 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home-view.component.scss'
 })
 export class HomeViewComponent {
+  @ViewChild('instagramSlider')
+instagramSlider!: ElementRef<HTMLDivElement>;
+
+slideInstagram(direction: 'next' | 'prev') {
+  const slider = this.instagramSlider.nativeElement;
+  const distance = slider.clientWidth;
+
+  slider.scrollBy({
+    left: direction === 'next' ? distance : -distance,
+    behavior: 'smooth'
+  });
+}
   products = [
-    { name: 'הידרוסול - מי ורדים', price: '₪44.00', reviews: 22, image: '/images/home/product-1.png' },
-    { name: 'חומצה היאלורונית ננו', price: '₪164.00', reviews: 29, image: '/images/home/product-2.png' },
-    { name: 'תמצית CO2 גרגרים', price: '₪90.00', reviews: 16, image: '/images/home/product-3.png' },
-    { name: 'Citron oil - שמן אתרוגים', price: '₪54.00', reviews: 22, image: '/images/home/product-4.png' },
-    { name: 'HYALURONIC ACID', price: '₪95.00', reviews: 52, image: '/images/home/product-5.png' }
+    { name: 'הידרוסול - מי ורדים', price: '₪44.00', reviews: 22, image: '/images/home/1.jpg' },
+    { name: 'חומצה היאלורונית ננו', price: '₪164.00', reviews: 29, image: '/images/home/2.jpg' },
+    { name: 'תמצית CO2 גרגרים', price: '₪90.00', reviews: 16, image: '/images/home/3.jpg' },
+    { name: 'Citron oil - שמן אתרוגים', price: '₪54.00', reviews: 22, image: '/images/home/4.jpg' },
+    { name: 'HYALURONIC ACID', price: '₪95.00', reviews: 52, image: '/images/home/5.jpg' }
   ];
 
   posts = [
-    { title: 'חומצה סליצילית לפנים – מה היא עושה?', image: '/images/home/blog-1.jpg' },
-    { title: 'ניאצינמיד וויטמין C – האם אפשר לשלב?', image: '/images/home/blog-2.jpg' },
-    { title: 'אלפא ארבוטין – חומר גלם שמבהיר כתמים', image: '/images/home/blog-3.jpg' }
+    { title: 'חומצה סליצילית לפנים – מה היא עושה?', image: '/images/home/6.jpg' },
+    { title: 'ניאצינמיד וויטמין C – האם אפשר לשלב?', image: '/images/home/7.jpg' },
+    { title: 'אלפא ארבוטין – חומר גלם שמבהיר כתמים', image: '/images/home/8.jpg' }
   ];
 
   cards = [
-    { title: 'מתחילה בעולם הקוסמטיקה הטבעית?', image: '/images/home/card-1.jpg', button: 'לקריאה במאמר' },
-    { title: 'מחפשת רעיון מקורי להפעלה או סדנה?', image: '/images/home/card-2.jpg', button: 'לפרטים נוספים' },
-    { title: 'קורס קוסמטיקה טבעית - פרונטלי', image: '/images/home/card-3.jpg', button: 'לפרטים נוספים' }
+    { title: 'מתחילה בעולם הקוסמטיקה הטבעית?', image: '/images/home/9.jpg', button: 'לקריאה במאמר' },
+    { title: 'מחפשת רעיון מקורי להפעלה או סדנה?', image: '/images/home/10.jpg', button: 'לפרטים נוספים' },
+    { title: 'קורס קוסמטיקה טבעית - פרונטלי', image: '/images/home/11.jpg', button: 'לפרטים נוספים' }
   ];
 
   instagram = [
-    '/images/home/blog-1.jpg',
-    '/images/home/blog-2.jpg',
-    '/images/home/blog-3.jpg',
-    '/images/home/blog-main.jpg'
+    '/images/home/12.jpg',
+    '/images/home/13.jpg',
+    '/images/home/14.jpg',
+    '/images/home/15.jpg'
   ];
 }
